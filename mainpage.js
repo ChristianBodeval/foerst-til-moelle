265ad65dd0a6894620e1139d5f2f1e592b3fc46e
Christian Bodeval <chrille009@gmail.com>
2023-10-28 21:32:54 +0200

    push

---
+++ mainpage.js
@@ -0,0 +1,8 @@
+// Get the button element
+var annonceButton = document.querySelector('.createAnnonceButton');
+
+// Add click event listener
+annonceButton.addEventListener('click', function() {
+    // Print a message to the console
+    console.log('Button clicked!');
+});
