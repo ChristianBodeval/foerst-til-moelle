1e0e2ac86fedaa7d1457f3388be0c4404dd306f1
Christian Bodeval <chrille009@gmail.com>
2023-10-30 19:06:06 +0100

    Added items on main menu

--- addAnnoncePage.js
+++ addAnnoncePage.js
@@ -18,3 +18,12 @@ annonceButton.addEventListener('click', function() {
     event.preventDefault();
     window.location.href = "mainpage.html"; // Change to the desired URL
 });
+
+var annonceButton = document.querySelector('.leftRegionRankingUserlistButton');
+
+// Add click event listener
+annonceButton.addEventListener('click', function() {
+    // Print a message to the console
+    event.preventDefault();
+    window.location.href = "userList.html"; // Change to the desired URL
+});
--- mainpage.js
+++ mainpage.js
@@ -8,3 +8,13 @@ annonceButton.addEventListener('click', function() {
     window.location.href = "addAnnoncePage.html"; // Change to the desired URL
 
 });
+
+
+var annonceButton = document.querySelector('.leftRegionRankingUserlistButton');
+
+// Add click event listener
+annonceButton.addEventListener('click', function() {
+    // Print a message to the console
+    event.preventDefault();
+    window.location.href = "userList.html"; // Change to the desired URL
+});
